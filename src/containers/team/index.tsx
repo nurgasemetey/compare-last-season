a0ab02dd9d6dde13a264d3da0e25dd8376c7e87a
Nurgazy Nazhimidinov <nurgasemetey@gmail.com>
2020-11-12 13:25:49 +0300

    fix this season

--- src/containers/team/index.tsx
+++ src/containers/team/index.tsx
@@ -16,7 +16,8 @@ interface IProps extends RouteComponentProps<RouterProps> { }
 
 
 interface IState {
-  lastSeasonData: any[]
+  lastSeasonData: any[],
+  thisSeasonData: any[],
   pointChangeData: any[],
 }
 
@@ -37,8 +38,8 @@ const matchColumns = [
     dataIndex: 'homeScore',
     key: 'homeScore',
     render(value: any, record: any) {
-      if (value) {
-        console.log(value);
+      if (value.score && value.score.ft) {
+        // console.log(value);
         const score = value.score.ft;
         let color = "white";
         if (score[0] > score[1]) {
@@ -65,8 +66,8 @@ const matchColumns = [
     dataIndex: 'awayScore',
     key: 'awayScore',
     render(value: any, record: any) {
-      if (value) {
-        console.log(value);
+      if (value.score && value.score.ft) {
+        // console.log(value);
         const score = value.score.ft;
         let color = "white";
         if (score[0] < score[1]) {
@@ -87,124 +88,6 @@ const matchColumns = [
         };
       }
     }
-  }
-];
-
-const thisSeasonData = [
-  {
-    key: '1',
-    name: 'Liverpool',
-    homeScore: '0-2',
-    awayScore: ''
-  },
-  {
-    key: '2',
-    name: 'City',
-    homeScore: '',
-    awayScore: ''
-  },
-  {
-    key: '3',
-    name: 'United',
-    homeScore: '',
-    awayScore: '0-0'
-  },
-  {
-    key: '4',
-    name: 'Leicester',
-    homeScore: '',
-    awayScore: ''
-  },
-  {
-    key: '5',
-    name: 'Spurs',
-    homeScore: '',
-    awayScore: ''
-  },
-  {
-    key: '6',
-    name: 'Wolves',
-    homeScore: '',
-    awayScore: ''
-  },
-  {
-    key: '7',
-    name: 'Arsenal',
-    homeScore: '',
-    awayScore: ''
-  },
-  {
-    key: '8',
-    name: 'Sheffield',
-    homeScore: '4-1',
-    awayScore: '',
-    isNew:true
-  },
-  {
-    key: '9',
-    name: 'Burnley',
-    homeScore: '',
-    awayScore: '0-3'
-  },
-  {
-    key: '10',
-    name: 'Southampton',
-    homeScore: '3-3',
-    awayScore: ''
-  },
-  {
-    key: '11',
-    name: 'Everton',
-    homeScore: '',
-    awayScore: ''
-  },
-  {
-    key: '12',
-    name: 'Newcastle',
-    homeScore: '',
-    awayScore: ''
-  },
-  {
-    key: '13',
-    name: 'Crystal Palace',
-    homeScore: '4-0',
-    awayScore: ''
-  },
-  {
-    key: '14',
-    name: 'Brighton',
-    homeScore: '',
-    awayScore: '1-3'
-  },
-  {
-    key: '15',
-    name: 'Westham',
-    homeScore: '',
-    awayScore: ''
-  },
-  {
-    key: '16',
-    name: 'Aston Villa',
-    homeScore: '',
-    awayScore: ''
-  },
-  {
-    key: '17',
-    name: 'Leeds',
-    homeScore: '',
-    awayScore: ''
-  },
-  {
-    key: '18',
-    name: 'West Brom',
-    homeScore: '',
-    awayScore: '3-3'
-  },
-  {
-    key: '19',
-    name: 'Fulham',
-    homeScore: '',
-    awayScore: ''
   }
 ];
 
@@ -267,69 +150,120 @@ export class TeamContainer extends React.Component<IProps, IState> {
 
   state = {
     lastSeasonData: [],
-    pointChangeData: []
+    pointChangeData: [],
+    thisSeasonData: []
   }
 
-  componentDidMount = async () => {
-    console.log(this.props.match.params.leagueId, this.props.match.params.teamId);
+  componentDidMount = async () => { 
+    try {
+      console.log(this.props.match.params.leagueId, this.props.match.params.teamId);
 
-    const teamId = this.props.match.params.teamId;
-
-    const thisSeasonClubs = require(`../../assets/data/${THIS_SEASON}/${this.props.match.params.leagueId}.clubs.json`).clubs.sort((a:any, b:any) => (a.name > b.name) ? 1 : -1);
-    const lastSeasonClubs = require(`../../assets/data/${LAST_SEASON}/${this.props.match.params.leagueId}.clubs.json`).clubs.sort((a:any, b:any) => (a.name > b.name) ? 1 : -1);
-
-    // console.log(thisSeasonsClubs);
-
-    let commonClubs = _.intersectionBy(lastSeasonClubs, thisSeasonClubs, 'name').filter((element:any) => element.name !== teamId);
-
-    let lastSeasonDemotedTeams = _.differenceBy(lastSeasonClubs, thisSeasonClubs, 'name');
-    // console.log(lastSeasonDemotedTeams);
-
-    let thisSeasonPromotedTeams = _.differenceBy(thisSeasonClubs, lastSeasonClubs, 'name');
-    // console.log(thisSeasonPromotedTeams);
-
-    const lastSeasonMatches = require(`../../assets/data/${LAST_SEASON}/${this.props.match.params.leagueId}.json`).matches.filter((a:any)=> {
-      return a.team1 === teamId || a.team2 === teamId;
-    });
-
-    let lastSeasonMatchesMap: Map<string, any[]> = new Map();
-    // console.log(lastSeasonMatches);
-
-    for (let index = 0; index < lastSeasonMatches.length; index++) {
-      const element = lastSeasonMatches[index];
-      let key = 'team1';
-      if(element.team1 === teamId) {
-        key = 'team2';
-      } 
-      if(lastSeasonMatchesMap.has(element[key])) {
-        let matches:any[] = lastSeasonMatchesMap.get(element[key]) || [];
-        matches?.push(element);
-        lastSeasonMatchesMap.set(element[key], matches);
+      const teamId = this.props.match.params.teamId;
+  
+      const thisSeasonClubs = require(`../../assets/data/${THIS_SEASON}/${this.props.match.params.leagueId}.clubs.json`).clubs.sort((a:any, b:any) => (a.name > b.name) ? 1 : -1);
+      const lastSeasonClubs = require(`../../assets/data/${LAST_SEASON}/${this.props.match.params.leagueId}.clubs.json`).clubs.sort((a:any, b:any) => (a.name > b.name) ? 1 : -1);
+  
+      // console.log(thisSeasonsClubs);
+  
+      let commonClubs = _.intersectionBy(lastSeasonClubs, thisSeasonClubs, 'name').filter((element:any) => element.name !== teamId);
+  
+      let lastSeasonDemotedTeams = _.differenceBy(lastSeasonClubs, thisSeasonClubs, 'name');
+      // console.log(lastSeasonDemotedTeams);
+  
+      let thisSeasonPromotedTeams = _.differenceBy(thisSeasonClubs, lastSeasonClubs, 'name');
+      // console.log(thisSeasonPromotedTeams);
+  
+      const lastSeasonMatches = require(`../../assets/data/${LAST_SEASON}/${this.props.match.params.leagueId}.json`).matches.filter((a:any)=> {
+        return a.team1 === teamId || a.team2 === teamId;
+      });
+  
+      let lastSeasonMatchesMap: Map<string, any[]> = new Map();
+      // console.log(lastSeasonMatches);
+  
+      for (let index = 0; index < lastSeasonMatches.length; index++) {
+        const element = lastSeasonMatches[index];
+        let key = 'team1';
+        if(element.team1 === teamId) {
+          key = 'team2';
+        } 
+        if(lastSeasonMatchesMap.has(element[key])) {
+          let matches:any[] = lastSeasonMatchesMap.get(element[key]) || [];
+          matches?.push(element);
+          lastSeasonMatchesMap.set(element[key], matches);
+        }
+        else {
+          lastSeasonMatchesMap.set(element[key], [element]);
+        }
       }
-      else {
-        lastSeasonMatchesMap.set(element[key], [element]);
+      let lastSeasonData: any[] = [];
+      // console.log(lastSeasonMatchesMap);
+      for (let index = 0; index < commonClubs.length; index++) {
+        const element:any = _.cloneDeep(commonClubs[index]);
+        // console.log(element.name);
+        let matches:any[] = lastSeasonMatchesMap.get(element.name) || [];
+        // console.log(matches);
+        if(matches[0].team1 === teamId) {
+          element.homeScore = matches[0];
+          element.awayScore = matches[1];
+        }
+        else {
+          element.homeScore = matches[1];
+          element.awayScore = matches[0];
+        }
+        lastSeasonData.push(element)
       }
+      this.setState({ lastSeasonData });
+  
+      //---------
+      const thisSeasonMatches = require(`../../assets/data/${THIS_SEASON}/${this.props.match.params.leagueId}.json`).matches.filter((a:any)=> {
+        return a.team1 === teamId || a.team2 === teamId;
+      });
+  
+      // console.log(thisSeasonMatches);
+      
+      let thisSeasonMatchesMap: Map<string, any[]> = new Map();
+      // console.log(lastSeasonMatches);
+  
+      for (let index = 0; index < thisSeasonMatches.length; index++) {
+        const element = thisSeasonMatches[index];
+        let key = 'team1';
+        if(element.team1 === teamId) {
+          key = 'team2';
+        } 
+        if(thisSeasonMatchesMap.has(element[key])) {
+          let matches:any[] = thisSeasonMatchesMap.get(element[key]) || [];
+          matches?.push(element);
+          thisSeasonMatchesMap.set(element[key], matches);
+        }
+        else {
+          thisSeasonMatchesMap.set(element[key], [element]);
+        }
+      }
+      let thisSeasonData: any[] = [];
+      // console.log(lastSeasonMatchesMap);
+      for (let index = 0; index < commonClubs.length; index++) {
+        const element:any = _.cloneDeep(commonClubs[index]);
+        // console.log(element.name);
+        let matches:any[] = thisSeasonMatchesMap.get(element.name) || [];
+        // console.log(matches);
+        if(matches[0].team1 === teamId) {
+          element.homeScore = matches[0];
+          element.awayScore = matches[1];
+        }
+        else {
+          element.homeScore = matches[1];
+          element.awayScore = matches[0];
+        }
+        thisSeasonData.push(element)
+      }
+      console.log("thisSEason", thisSeasonData);
+
+      this.setState({ thisSeasonData });
+
+    } catch(err) {
+      console.log(err);
     }
-    let lastSeasonData: any[] = [];
-    // console.log(lastSeasonMatchesMap);
-    for (let index = 0; index < commonClubs.length; index++) {
-      const element:any = _.cloneDeep(commonClubs[index]);
-      // console.log(element.name);
-      let matches:any[] = lastSeasonMatchesMap.get(element.name) || [];
-      // console.log(matches);
-      if(matches[0].team1 === teamId) {
-        element.homeScore = matches[0];
-        element.awayScore = matches[1];
-      }
-      else {
-        element.homeScore = matches[1];
-        element.awayScore = matches[0];
-      }
-      lastSeasonData.push(element)
-    }
-    console.log(lastSeasonData);
-    this.setState({ lastSeasonData })
-
+    
 
 
     //--------------------------
@@ -403,7 +337,7 @@ export class TeamContainer extends React.Component<IProps, IState> {
   }
 
   render() {
-    const { lastSeasonData, pointChangeData } = this.state;
+    const { lastSeasonData, thisSeasonData, pointChangeData } = this.state;
     return (
       <>
         <PageHeader
@@ -505,63 +439,63 @@ export class TeamContainer extends React.Component<IProps, IState> {
             lg={{ span: 9 }}
             xl={{ span: 9 }}
           >
-            {/* <Table 
+            <Table 
               title={(data:any)=> `This season ${THIS_SEASON}`} 
               bordered 
               columns={matchColumns} 
               dataSource={thisSeasonData} 
               pagination={false} 
               size="small"
-              summary={pageData => {
-                let totalHome = 0;
-                let totalAway = 0;
+              // summary={pageData => {
+              //   let totalHome = 0;
+              //   let totalAway = 0;
         
-                pageData.forEach(({ homeScore, awayScore }) => {
-                  if(homeScore) {
-                    const homeScoreArr: string[] = homeScore.split("-");
-                    if (parseInt(homeScoreArr[0]) > parseInt(homeScoreArr[1])) {
-                      totalHome += 3;
-                    }
-                    else if (parseInt(homeScoreArr[0]) < parseInt(homeScoreArr[1])) {
-                      totalHome += 0;
-                    }
-                    else {
-                      totalHome += 1;
-                    }
-                  }
+              //   pageData.forEach(({ homeScore, awayScore }) => {
+              //     if(homeScore) {
+              //       const homeScoreArr: string[] = homeScore.split("-");
+              //       if (parseInt(homeScoreArr[0]) > parseInt(homeScoreArr[1])) {
+              //         totalHome += 3;
+              //       }
+              //       else if (parseInt(homeScoreArr[0]) < parseInt(homeScoreArr[1])) {
+              //         totalHome += 0;
+              //       }
+              //       else {
+              //         totalHome += 1;
+              //       }
+              //     }
                   
-                  if(awayScore) {
-                    const awayScoreArr: string[] = awayScore.split("-");
-                    if (parseInt(awayScoreArr[0]) > parseInt(awayScoreArr[1])) {
-                      totalAway += 0;
-                    }
-                    else if (parseInt(awayScoreArr[0]) < parseInt(awayScoreArr[1])) {
-                      totalAway += 3;
-                    }
-                    else {
-                      totalAway += 1;
-                    }
-                  }
+              //     if(awayScore) {
+              //       const awayScoreArr: string[] = awayScore.split("-");
+              //       if (parseInt(awayScoreArr[0]) > parseInt(awayScoreArr[1])) {
+              //         totalAway += 0;
+              //       }
+              //       else if (parseInt(awayScoreArr[0]) < parseInt(awayScoreArr[1])) {
+              //         totalAway += 3;
+              //       }
+              //       else {
+              //         totalAway += 1;
+              //       }
+              //     }
                   
-                });
+              //   });
         
-                return (
-                  <>
-                    <Table.Summary.Row>
-                      <Table.Summary.Cell align="center" index={0}>
-                        <Text strong>Total</Text>
-                      </Table.Summary.Cell>
-                      <Table.Summary.Cell align="center" index={1}>
-                        <Text strong>{totalHome}<Text type="danger">(+3)</Text></Text>
-                      </Table.Summary.Cell>
-                      <Table.Summary.Cell align="center" index={2}>
-                        <Text strong>{totalAway}</Text>
-                      </Table.Summary.Cell>
-                    </Table.Summary.Row>
-                  </>
-                );
-              }}
-            /> */}
+              //   return (
+              //     <>
+              //       <Table.Summary.Row>
+              //         <Table.Summary.Cell align="center" index={0}>
+              //           <Text strong>Total</Text>
+              //         </Table.Summary.Cell>
+              //         <Table.Summary.Cell align="center" index={1}>
+              //           <Text strong>{totalHome}<Text type="danger">(+3)</Text></Text>
+              //         </Table.Summary.Cell>
+              //         <Table.Summary.Cell align="center" index={2}>
+              //           <Text strong>{totalAway}</Text>
+              //         </Table.Summary.Cell>
+              //       </Table.Summary.Row>
+              //     </>
+              //   );
+              // }}
+            />
           </Col>
 
           <Col
